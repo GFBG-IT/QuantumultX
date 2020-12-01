6e3566fa5510230b16c9a47baf47c5aed1536f89
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2020-12-01 23:18:13 +0800

    Create rrtv.js

---
+++ rrtv.js
@@ -0,0 +1,21 @@
+/* 
+
+我的app版本4.4.1
+
+# QuanX
+[rewrite_local]
+# 人人视频 旧版本api限制解除
+^https:\/\/api\.rr\.tv url script-request-header https://raw.githubusercontent.com/id77/QuantumultX/master/Script/rrtv.js
+
+*/
+
+const reg = /4\.\d+\.\d/g;
+const headers = JSON.parse(
+  JSON.stringify($request.headers).replace(reg, '4.16.5')
+);
+
+const data = { headers };
+
+data.url = $request.url.replace(reg, '4.16.5');
+
+$done(data);
